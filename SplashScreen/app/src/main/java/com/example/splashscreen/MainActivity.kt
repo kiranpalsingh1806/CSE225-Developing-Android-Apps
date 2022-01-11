491715237b1e00fb780c025e6020dfd0818954f0
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2022-01-11 13:19:10 +0530

    Add Splash Screen and Scroll View

---
+++ SplashScreen/app/src/main/java/com/example/splashscreen/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, SplashScreenMain::class.java)
+            startActivity(intent)
+            finish()}, 5000)
+    }
+}
---
+++ SplashScreen/app/src/main/java/com/example/splashscreen/SplashScreenMain.kt
@@ -0,0 +1,13 @@
+package com.example.splashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashScreenMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+    }
+}
