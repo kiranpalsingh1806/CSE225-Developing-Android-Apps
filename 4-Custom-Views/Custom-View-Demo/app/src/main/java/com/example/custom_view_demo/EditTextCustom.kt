b9aeb70d29698c36580cf94e1985a17406a09a92
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2022-03-15 12:52:26 +0530

    Complete Custom Views Program

--- 4-Custom-Views/Custom-View-Demo/app/src/main/java/com/example/custom_view_demo/EditTextCustom.kt
+++ 4-Custom-Views/Custom-View-Demo/app/src/main/java/com/example/custom_view_demo/EditTextCustom.kt
@@ -1,7 +1,94 @@
 package com.example.custom_view_demo
 
+import android.content.Context
+import android.content.res.TypedArray
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.MotionEvent
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.content.res.TypedArrayUtils
 
-class EditTextCustom : AppCompatTextView {
+class EditTextCustom(context: Context, attrs: AttributeSet) : AppCompatEditText(context, attrs) {
+    private var sethint: String? = null;
+    private var setcolor = false
+
+    var darkButton: Drawable? = null
+    var lightButton:Drawable? = null
+
+    fun init() {
+        darkButton = ResourcesCompat.getDrawable(resources, R.drawable.ic_clear_black_24dp, null)
+        lightButton = ResourcesCompat.getDrawable(resources, R.drawable.ic_clear_black_22dp, null)
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                showButton()
+                show()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+    }
+
+    fun show() {
+        setOnTouchListener { view, motionEvent ->
+            var isClicked:Boolean = false
+
+            val clearButtonStart = (width - paddingEnd - darkButton!!.intrinsicWidth).toFloat()
+            if(motionEvent.x  > clearButtonStart) {
+                isClicked = true
+            }
+
+            if(isClicked) {
+                when(motionEvent.action) {
+                    MotionEvent.ACTION_DOWN->text!!.clear()
+                    MotionEvent.ACTION_UP->hideButton()
+                }
+            }
+            true
+        }
+    }
+
+    fun showButton() {
+        setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, darkButton, null)
+    }
+
+    fun hideButton() {
+        setCompoundDrawablesRelativeWithIntrinsicBounds(null,  null, lightButton, null)
+    }
+
+    init {
+        val tarry1: TypedArray = context.obtainStyledAttributes(attrs, R.styleable.EditTextCustom)
+        try {
+            sethint = tarry1.getString(R.styleable.EditTextCustom_setHint)
+
+            if(sethint == null) {
+                setHint("Enter your message")
+            } else {
+                setHint("Enter your message here")
+            }
+
+            setcolor = tarry1.getBoolean(R.styleable.EditTextCustom_setColor1, false)
+
+            if(setcolor == true) {
+                setTextColor(Color.RED)
+            }
+        }
+        finally {
+            tarry1.recycle()
+            init()
+        }
+    }
+
 
 }
--- 4-Custom-Views/Custom-View-Demo/app/src/main/java/com/example/custom_view_demo/TextViewCustom.kt
+++ 4-Custom-Views/Custom-View-Demo/app/src/main/java/com/example/custom_view_demo/TextViewCustom.kt
@@ -3,18 +3,20 @@ package com.example.custom_view_demo
 import android.content.Context
 import android.content.res.TypedArray
 import android.graphics.Color
+import android.graphics.Typeface
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatTextView
 
 class TextViewCustom : AppCompatTextView {
     private var title : String? = null
     private var color = false;
+    private var italic = false;
 
     constructor(context : Context) : super(context) {}
     constructor(context : Context, attrs : AttributeSet) : super(context, attrs) {
         val tarry : TypedArray = context.obtainStyledAttributes(attrs, R.styleable.TextViewCustom)
         try {
-            title = tarry.getString(R.styleable.TextViewCustom_settitle)
+            title = tarry.getString(R.styleable.TextViewCustom_setTitle)
             if(title == null) {
                 setText("Custom Message")
             } else {
@@ -25,9 +27,16 @@ class TextViewCustom : AppCompatTextView {
             if(color) {
                 setTextColor(Color.MAGENTA)
             }
+
+            italic = tarry.getBoolean(R.styleable.TextViewCustom_setItalic, false)
+            if(italic) {
+                setTypeface(null , Typeface.ITALIC)
+            }
         }
         finally {
             tarry.recycle()
         }
     }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
 }
