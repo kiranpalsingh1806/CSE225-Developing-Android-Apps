bc8550e38054c8eb6235d86d5b981dfc33e5af70
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2022-03-16 13:42:00 +0530

    Add Custom View more attributes

--- 4-Custom-Views/Custom-View-Demo/app/src/main/java/com/example/custom_view_demo/TextViewCustom.kt
+++ 4-Custom-Views/Custom-View-Demo/app/src/main/java/com/example/custom_view_demo/TextViewCustom.kt
@@ -3,14 +3,19 @@ package com.example.custom_view_demo
 import android.content.Context
 import android.content.res.TypedArray
 import android.graphics.Color
+import android.graphics.Color.parseColor
 import android.graphics.Typeface
 import android.util.AttributeSet
+import android.util.Log
 import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.graphics.toColorInt
 
 class TextViewCustom : AppCompatTextView {
     private var title : String? = null
     private var color = false;
     private var italic = false;
+    private var allCapital = false;
+    private var writeColor : String? = null
 
     constructor(context : Context) : super(context) {}
     constructor(context : Context, attrs : AttributeSet) : super(context, attrs) {
@@ -20,18 +25,32 @@ class TextViewCustom : AppCompatTextView {
             if(title == null) {
                 setText("Custom Message")
             } else {
-                setText("Hello")
+                setText("Kiranpal Singh")
+            }
+
+            writeColor = tarry.getString(R.styleable.TextViewCustom_typeColor)
+            if(writeColor == null) {
+                setTextColor(Color.BLUE)
+            } else {
+                Log.i("What is that ?", writeColor.toString())
+                setTextColor(writeColor!!.toColorInt())
             }
 
             color = tarry.getBoolean(R.styleable.TextViewCustom_setColor, false)
             if(color) {
-                setTextColor(Color.MAGENTA)
+                setTextColor(Color.BLUE)
             }
 
             italic = tarry.getBoolean(R.styleable.TextViewCustom_setItalic, false)
             if(italic) {
                 setTypeface(null , Typeface.ITALIC)
             }
+
+            allCapital = tarry.getBoolean(R.styleable.TextViewCustom_makeAllCapital, false)
+            if(allCapital) {
+                setText(text.toString().uppercase())
+            }
+
         }
         finally {
             tarry.recycle()
