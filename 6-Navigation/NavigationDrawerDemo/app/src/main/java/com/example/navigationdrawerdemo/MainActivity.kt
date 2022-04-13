2590850653dccfaf49a6ebf48e6164099f595937
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2022-04-13 12:55:11 +0530

    Add navigation drawer fragments and kotlin files

--- 6-Navigation/NavigationDrawerDemo/app/src/main/java/com/example/navigationdrawerdemo/MainActivity.kt
+++ 6-Navigation/NavigationDrawerDemo/app/src/main/java/com/example/navigationdrawerdemo/MainActivity.kt
@@ -2,12 +2,48 @@ package com.example.navigationdrawerdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    lateinit var drawer : DrawerLayout
+    lateinit var navigationView : NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var toolbar : Toolbar = findViewById(R.id.toolbar)
+        navigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        var actionBarDrawerToggle = ActionBarDrawerToggle(this, drawer, toolbar,
+        R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+
+        drawer.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        if(savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, fragment_message()).commit()
+            navigationView.setCheckedItem(R.id.new_message)
+        }
+    }
+
+    override fun onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+        super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
 
     }
 }
